2a382817cbe69c6eae737d9039a5556ee95bd060
kevin.perettimalaguti <kevin.peretti-malaguti@laplateforme.io>
2023-12-04 13:29:42 +0100

    reparation couleur code

--- pendu.py
+++ pendu.py
@@ -1,14 +1,7 @@
-import pygame 
+import pygame
 
-surf = pygame.display.set_mode((800,600))
-run = True
-posX = 50
-while run :
-  for event in pygame.event.get():
-    if event.type == pygame.QUIT:
-      run = False
-  surf.fill((0,0,0))
-  pygame.draw.circle(surf, (255,0,0), (posX, 300), 30, 2)
-  posX = posX + 1
-  pygame.display.flip()
+pygame.init()
+
+ecran_de_jeu = pygame.display.set_mode((800,600))
+
 pygame.quit()
