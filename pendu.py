0803a8ba0f6b771e7849757141d3ab52cc871ea2
kevin.perettimalaguti <kevin.peretti-malaguti@laplateforme.io>
2023-12-05 17:37:12 +0100

    progression 75% pendu

--- pendu.py
+++ pendu.py
@@ -25,13 +25,12 @@ police_option = pygame.font.Font(None, 21)
 with open("mots.txt", 'r') as fichier_mots:
     mots = [mot.strip() for mot in fichier_mots.readlines()]
     mot_random = random.choice(mots)
-    
+
 # Mot à deviner
 mot_affiche = ['_' for _ in mot_random]
-    
+
 # Esthétique avant le jeu
-texte_presentation = "Appuyez sur une touche pour jouer"
-option_rajout = "cliquer pour ajouter un mot"
+texte_presentation = "Que voulez faire ?"
 
 # Fonction pour afficher le mot dans la fenêtre
 def afficher_mot():
@@ -42,50 +41,73 @@ def afficher_mot():
         affichage_lettre = police_menu.render(lettre, True, red)
         ecran_de_jeu.blit(affichage_lettre, (x, y))
         x += affichage_lettre.get_width() + espacement_entre_lettres
-    
 
 # Affichage du menu
-ecran_de_jeu.fill(grey)
-affichage_texte = police_menu.render(texte_presentation, True, red)
-ecran_de_jeu.blit(affichage_texte, (largeur // 2 - affichage_texte.get_width() // 2, hauteur // 2))
-option_ajout_mot = police.render(option_rajout, True, red )
-ecran_de_jeu.blit(option_ajout_mot, (largeur // 1.2 - option_ajout_mot.get_width() // 1.2, hauteur // 1.2))
-pygame.display.flip()
+def afficher_menu(selectionner_option):
+    ecran_de_jeu.fill(grey)
+    affichage_texte = police_menu.render(texte_presentation, True, red)
+    ecran_de_jeu.blit(affichage_texte, (largeur // 2 - affichage_texte.get_width() // 2, hauteur // 2))
+
+    options = ["Jouer", "Ajouter un mot", "Quitter"]
+    for i, option in enumerate(options):
+        
+        if i == selectionner_option:
+            couleur = red              
+        else:
+            couleur = white
+        affichage_option = police_menu.render(option, True, couleur)
+        ecran_de_jeu.blit(affichage_option, (largeur // 2 - affichage_option.get_width() // 2, 100 + i * 50))
+
+    pygame.display.flip()
 
 # Boucle principale
 en_jeu = False
+erreur = 0
+selected_option = 0
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()            
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                selected_option = (selected_option - 1) % 3
+            elif event.key == pygame.K_DOWN:
+                selected_option = (selected_option + 1) % 3
+            elif event.key == pygame.K_RETURN:
+                options = ["Jouer", "Ajouter un mot", "Quitter"]  # Ajout de cette ligne pour définir options
+                if options[selected_option] == "Jouer":
+                    en_jeu = True
+                    ecran_de_jeu.fill(grey)
+                    afficher_mot()
+                    pygame.display.flip()
+                elif options[selected_option] == "Ajouter un mot":
+                    nouveau_mot = input("Entrez un nouveau mot : ")
+                    with open("mots.txt", 'a') as fichier:
+                        fichier.write(nouveau_mot + "\n")
+                    print("Mot ajouté avec succès !")
+                elif options[selected_option] == "Quitter":
+                    pygame.quit()
+                    
 
-        elif event.type == pygame.KEYDOWN and not en_jeu:
-            # Passage au jeu lorsqu'une touche est pressée
-            en_jeu = True
-            ecran_de_jeu.fill(grey)
-            afficher_mot()
-            pygame.display.flip()
-        
-        elif event.type == pygame.MOUSEBUTTONDOWN and not en_jeu:
-            en_jeu = False
-            nouveau_mot = input("Entrez un nouveau mot : ")
-            with open("mots.txt", 'a') as fichier:       
-                fichier.write(nouveau_mot + "\n")  # On écrit le mot que l'on veut rajouter
+    if en_jeu:
+        # Logique du jeu
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                if event.unicode.isalpha():
+                    lettre = event.unicode.lower()
+                    if lettre in mot_random:
+                        # Remplacer les '_' par la lettre correcte
+                        for position, position_lettre in enumerate(mot_random):
+                            if position_lettre == lettre:
+                                mot_affiche[position] = lettre
+               
 
-        elif event.type == pygame.KEYDOWN and en_jeu:
-            if event.unicode.isalpha():
-                lettre = event.unicode.lower()
-                if lettre in mot_random:
-                    # Remplacer les '_' par la lettre correcte
-                    for position, position_lettre in enumerate(mot_random):
-                        if position_lettre == lettre:
-                            mot_affiche[position] = lettre 
+        # Mise à jour de l'affichage du mot
+        ecran_de_jeu.fill(grey)
+        afficher_mot()
+        pygame.display.flip()        
 
-            # Mise à jour de l'affichage du mot
-            ecran_de_jeu.fill(grey)
-            afficher_mot()
-            pygame.display.flip() 
-            
-  
-              
+    else:
+        afficher_menu(selected_option)
+
